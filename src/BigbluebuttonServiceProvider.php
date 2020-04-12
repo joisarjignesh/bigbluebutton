30aaed63a41e5ee6b40e2c5a0a540126e5704f36
Jignesh <jigneshjoisar@gmail.com>
2020-04-12 17:26:08 +0530

    update compser and vendor publish

--- src/BigbluebuttonServiceProvider.php
+++ src/BigbluebuttonServiceProvider.php
@@ -12,36 +12,10 @@ class BigbluebuttonServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'bigbluebutton');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'bigbluebutton');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('bigbluebutton.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/bigbluebutton'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/bigbluebutton'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/bigbluebutton'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
+            ], 'bigbluebutton-config');
         }
     }
 
@@ -50,7 +24,6 @@ class BigbluebuttonServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'bigbluebutton');
 
         $server_base_url = $this->app['config']->get('bigbluebutton.BBB_SERVER_BASE_URL');
@@ -58,7 +31,7 @@ class BigbluebuttonServiceProvider extends ServiceProvider
 
         putenv("BBB_SERVER_BASE_URL=$server_base_url");
         putenv("BBB_SECURITY_SALT=$server_salt");
-        // Register the main class to use with the facade
+
         $this->app->bind('Bigbluebutton', function () {
             return new Bbb(new BigBlueButton());
         });
