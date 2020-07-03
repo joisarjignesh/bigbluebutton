17d87cb694e924992d13f9b13d20598aaa4d22ca
Jignesh <jigneshjoisar@gmail.com>
2020-07-03 21:13:14 +0530

    added new test

---
+++ tests/GetApiVersionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JoisarJignesh\Bigbluebutton\Tests;
+
+use JoisarJignesh\Bigbluebutton\Facades\Bigbluebutton;
+use Orchestra\Testbench\TestCase;
+use JoisarJignesh\Bigbluebutton\BigbluebuttonServiceProvider;
+
+class GetApiVersionTest extends TestCase
+{
+
+    protected function getPackageProviders($app)
+    {
+        return [BigbluebuttonServiceProvider::class];
+    }
+
+    protected function getApplicationAliases($app)
+    {
+        return Bigbluebutton::class;
+    }
+
+    /** @test */
+    public function true_is_true()
+    {
+        $array = [1];
+        $this->assertCount(1, $array);
+    }
+}
